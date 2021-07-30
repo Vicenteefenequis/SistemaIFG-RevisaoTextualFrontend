41fb24ca197b5de2c53fda33e067fe4118aa6e1a
Vicente Junior <vicente19981@live.com>
2021-07-30 17:00:01 -0300

    feat: added css base line and container max 100vw

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Router from "./Router";
+import { CssBaseline, Container } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <CssBaseline />
+    <Container maxWidth="100vw" className="main-container">
+      <Router />
+    </Container>
   </React.StrictMode>,
   document.getElementById("root")
 );
