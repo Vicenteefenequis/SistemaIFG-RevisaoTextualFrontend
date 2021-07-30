cd37269179aaa13a528cccb6251faaf2804d447b
Vicente Junior <vicente19981@live.com>
2021-07-30 17:00:53 -0300

    feat: create routes of sigin service and registre

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "../Screen/Home";
-import Header from "../Components/Header";
-import Footer from "../Components/Footer";
+
+import Servico from "../Screen/Servico";
+import SignIn from "../Screen/SignIn";
+import SignUp from "../Screen/SignUp";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Switch>
-        <Route path="/">
+        <Route path="/" exact>
           <Home />
         </Route>
+        <Route path="/servico">
+          <Servico />
+        </Route>
+        <Route path="/entrar">
+          <SignIn />
+        </Route>
+        <Route path="/registrar">
+          <SignUp />
+        </Route>
       </Switch>
-      <Footer />
     </BrowserRouter>
   );
 };
