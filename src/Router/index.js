e7fef04f77526fea5b70b7b81fef3a0e11893af6
Vicente Junior <vicente19981@live.com>
2021-07-31 01:13:38 -0300

    feat: change route in component

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,27 +1,22 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
+import Route from "./custom.route";
 import Home from "../Screen/Home";
 
 import Servico from "../Screen/Servico";
 import SignIn from "../Screen/SignIn";
 import SignUp from "../Screen/SignUp";
+import CentralCliente from "../Screen/CentralCliente";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/servico">
-          <Servico />
-        </Route>
-        <Route path="/entrar">
-          <SignIn />
-        </Route>
-        <Route path="/registrar">
-          <SignUp />
-        </Route>
+        <Route component={Home} path="/" exact />
+        <Route component={Servico} path="/servico" />
+        <Route component={CentralCliente} path="/cliente" isPrivate />
+        <Route component={SignIn} path="/entrar" />
+        <Route component={SignUp} path="/registrar" />
       </Switch>
     </BrowserRouter>
   );
