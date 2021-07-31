20fb26b5cc5c257defcb924b9160f9b82eb180f0
Vicente Junior <vicente19981@live.com>
2021-07-31 01:14:56 -0300

    feat: create helper of service

---
+++ src/Screen/Servico/helper.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+import { apiRevisaoTextual } from "../../config/api";
+
+function convertCurrency(valor) {
+  return new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  }).format(valor);
+}
+
+export function useServicoHelper() {
+  const [employs, setEmploys] = useState([]);
+
+  useEffect(() => {
+    apiRevisaoTextual.get("/employ").then((response) => {
+      const employers = response.data.data.map((res) => {
+        const valores = new Map();
+        valores.set("lauda", convertCurrency(res.valorLauda));
+        valores.set("palavra", convertCurrency(res.valorPalavra));
+        valores.set("pagina", convertCurrency(res.valorPagina));
+
+        return {
+          nome: res.nome,
+          descricao: res.descricao,
+          valores,
+        };
+      });
+      setEmploys(employers);
+    });
+  }, []);
+
+  return {
+    employs,
+  };
+}
