27fb1f2520bc0433bddd21cbec4356beebcd5fd6
Vicente Junior <vicente19981@live.com>
2021-08-06 22:50:55 -0300

    feat: create helper pagamento

---
+++ src/Screen/Pagamento/helper.js
@@ -0,0 +1,25 @@
+import { useCallback, useEffect, useState } from "react";
+import { apiRevisaoTextual } from "../../config/api";
+import { useBudget } from "../../hooks/useBudget";
+
+export function usePagamentoHelper(idOrcamento) {
+  const { boleto } = useBudget();
+  const [orcamento, setOrcamento] = useState({});
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = useCallback(() => {
+    setOpenModal(true);
+  }, []);
+  const handleCloseModal = useCallback(() => {
+    setOpenModal(false);
+  }, []);
+
+  useEffect(() => {
+    console.log("boleto", boleto);
+    apiRevisaoTextual.get(`/budget/${idOrcamento}`).then((response) => {
+      setOrcamento(response.data.data);
+    });
+  }, [idOrcamento]);
+
+  return { orcamento, boleto, handleOpenModal, handleCloseModal, openModal };
+}
