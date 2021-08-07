c5a89b53b5c279147526aecfadc2c83e98026e61
Vicente Junior <vicente19981@live.com>
2021-08-06 22:49:42 -0300

    feat: create route

--- src/Router/index.js
+++ src/Router/index.js
@@ -7,17 +7,22 @@ import Servico from "../Screen/Servico";
 import SignIn from "../Screen/SignIn";
 import SignUp from "../Screen/SignUp";
 import CentralCliente from "../Screen/CentralCliente";
+import Pagamento from "../Screen/Pagamento";
+import { BudgetContextProvider } from "../hooks/useBudget";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route component={Home} path="/" exact />
-        <Route component={Servico} path="/servico" />
-        <Route component={CentralCliente} path="/cliente" isPrivate />
-        <Route component={SignIn} path="/entrar" />
-        <Route component={SignUp} path="/registrar" />
-      </Switch>
+      <BudgetContextProvider>
+        <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={Servico} path="/servico" />
+          <Route component={CentralCliente} path="/cliente" isPrivate />
+          <Route component={Pagamento} path="/pagamento/:id" isPrivate />
+          <Route component={SignIn} path="/entrar" />
+          <Route component={SignUp} path="/registrar" />
+        </Switch>
+      </BudgetContextProvider>
     </BrowserRouter>
   );
 };
